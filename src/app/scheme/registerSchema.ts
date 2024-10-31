ca5b7301f721db3282367012e3cfc9fe26f280c1
lunaking1 <cabreltchoffo12@gmail.com>
2024-10-31 12:52:39 +0100

    create model scheme

--- src/app/scheme/registerSchema.ts
+++ src/app/scheme/registerSchema.ts
@@ -1,15 +1,10 @@
 import * as Yup from 'yup';
 
-export const registerSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, 'Name must be at least 2 characters')
-    .max(50, 'Name must be less than 50 characters')
-    .required('Name is required'),
-  email: Yup.string().email('Invalid email').required('Email is required'),
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters')
-    .required('Password is required'),
+export const registerSchema = Yup.object({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string().email('Invalid email format').required('Email is required'),
+  password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
   confirmPassword: Yup.string()
-    .oneOf([Yup.ref('password'), undefined], 'Passwords must match')
-    .required('Please confirm your password'),
+    .oneOf([Yup.ref('password'), ""], 'Passwords must match')
+    .required('Confirm Password is required'),
 });
