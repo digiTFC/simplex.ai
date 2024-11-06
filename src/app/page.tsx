d65f8c2e871e31f5a848adcd620d772d78fc3418
ledoux25 <sanguojoseph17@gmail.com>
2024-11-06 17:31:20 +0100

    SAAS-84-faq-component FaqLayout FaqBox Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
+import Faq from "./layouts/faq";
 
-import Contact from "./layouts/contactUs";
-import CopyRight from "./layouts/copyright";
-import Features from "./layouts/features";
-import Footer from "./layouts/footer";
-import Header from "./layouts/header";
-import NavBar from "./layouts/navBar";
-import Partners from "./layouts/partners";
-import Purpose from "./layouts/pupose";
 
 export default function Home() {
   return (
-    <div className=" w-full overflow-x-hidden">
-      <div><NavBar></NavBar></div>
-      <div><Header></Header></div>
-      <div><Features></Features></div>
-      <div><Purpose></Purpose></div>
-      <div><Partners></Partners></div>
-      <div><Contact></Contact></div>
-      <div><Footer></Footer></div>
-      <div>
-      <CopyRight></CopyRight>
-      </div>
+    <div>
+      <Faq></Faq>
     </div>
   );
 }
