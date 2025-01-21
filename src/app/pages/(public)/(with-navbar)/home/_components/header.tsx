f72ddb864b575f1e78324b3454187edbd6cbfd57
Cabrel Tchoffo <104843571+cabrelunreal@users.noreply.github.com>
2025-01-21 06:50:12 +0000

    home bannier text updated

--- src/app/pages/(public)/(with-navbar)/home/_components/header.tsx
+++ src/app/pages/(public)/(with-navbar)/home/_components/header.tsx
@@ -15,10 +15,10 @@ const Header = ({}) => {
         <div className=" curve-b relative  flex pt-16 md:pb-[300px] pb-[200px] -ml-[6vw] flex-col  z-40 bg-kwhiteBg dark:bg-black w-[112vw] ">
           <div className="relative w-10/12 md:w-1/2 m-auto  text-center">
             <Titles
-              title="Beautiful Landing Page Design for You"
+              title="From Idea to Impact – Launch Your Chatbot in Clicks"
               TitleStyle="text-[39px] md:text-[64px] font-semibold md:leading-[75px] m-auto md:max-w-[840px]"
               subtitleStyle="md:text-[18px] text-[14px] mt-[20px] mb-[37px] md:w-8/12 w-10/12  m-auto"
-              subTitle={`A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem `}
+              subTitle={`Tailored for African Innovators, Ready for the World, Turn Your Vision into Reality with Our Easy-to-Use Platform `}
             ></Titles>
 
             <Link href={"/pages/auth/signup"}>
